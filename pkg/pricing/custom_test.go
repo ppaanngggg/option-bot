4859e9718d16fd3f89d0430bc61eb4e80299d4e6
ht.pang <hantian.pang@gmail.com>
2024-02-06 23:22:49 +0800

    test tradier

--- pkg/pricing/custom_test.go
+++ pkg/pricing/custom_test.go
@@ -2,6 +2,7 @@ package pricing
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
@@ -97,8 +98,104 @@ func readOptionsDX() (*Chain, error) {
 	return &chain, nil
 }
 
-func TestCustom(t *testing.T) {
+func TestCustomOptionsDX(t *testing.T) {
 	chain, err := readOptionsDX()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(chain.Calls) != len(chain.Puts) {
+		t.Fatalf(
+			"calls and puts length mismatch: %d != %d", len(chain.Calls),
+			len(chain.Puts),
+		)
+	}
+	chain.SortByStrikePrice()
+	priceDistributions, err := chain.PredictPriceDistribution()
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, pd := range priceDistributions {
+		fmt.Printf("price: %0.4f, prob: %0.4f\n", pd.Price, pd.Prob)
+	}
+	for i := range chain.Calls {
+		call := chain.Calls[i]
+		callDelta := 0.0
+		for _, pd := range priceDistributions {
+			if pd.Price > call.StrikePrice {
+				callDelta += pd.Prob
+			}
+		}
+		put := chain.Puts[i]
+		putDelta := 0.0
+		for _, pd := range priceDistributions {
+			if pd.Price < put.StrikePrice {
+				putDelta += pd.Prob
+			}
+		}
+		fmt.Printf(
+			"strike: %0.4f, callDelta: %0.4f, putDelta: %0.4f\n",
+			call.StrikePrice, callDelta, putDelta,
+		)
+	}
+}
+
+type tradierOptions struct {
+	Options struct {
+		Option []struct {
+			Strike     float64 `json:"strike"`
+			Bid        float64 `json:"bid"`
+			BidSize    int     `json:"bidsize"`
+			Ask        float64 `json:"ask"`
+			AskSize    int     `json:"asksize"`
+			OptionType string  `json:"option_type"`
+		} `json:"option"`
+	} `json:"options"`
+}
+
+func readTradier() (*Chain, error) {
+	chain := Chain{
+		Symbol: "SPY",
+	}
+	file, err := os.Open("./test_data.json")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	var tradier tradierOptions
+	err = decoder.Decode(&tradier)
+	if err != nil {
+		return nil, err
+	}
+	for _, option := range tradier.Options.Option {
+		switch option.OptionType {
+		case "call":
+			chain.Calls = append(
+				chain.Calls, Option{
+					StrikePrice: option.Strike,
+					BidPrice:    option.Bid,
+					BidSize:     option.BidSize,
+					AskPrice:    option.Ask,
+					AskSize:     option.AskSize,
+				},
+			)
+		case "put":
+			chain.Puts = append(
+				chain.Puts, Option{
+					StrikePrice: option.Strike,
+					BidPrice:    option.Bid,
+					BidSize:     option.BidSize,
+					AskPrice:    option.Ask,
+					AskSize:     option.AskSize,
+				},
+			)
+		}
+	}
+	return &chain, nil
+}
+
+func TestCustomTradier(t *testing.T) {
+	chain, err := readTradier()
 	if err != nil {
 		t.Fatal(err)
 	}
