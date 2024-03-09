d6b7c7f8474b9011c3b659099741ae06a5854c55
ppaanngggg <hantian.pang@gmail.com>
2024-03-09 11:35:44 +0800

    support tradier market

--- pkg/market/market.go
+++ pkg/market/market.go
@@ -3,35 +3,37 @@ package market
 import (
 	"context"
 	"sort"
-	"time"
 )
 
 type Market interface {
-	GetOptionChains(ctx context.Context, symbol string, expiration string) (*Option, error)
+	GetOptionChains(ctx context.Context, symbol string, expiration string) ([]Chain, error)
 	GetOptionExpirations(ctx context.Context, symbol string) ([]string, error)
 }
 
 type Chain struct {
-	Symbol     string
-	QuoteTime  time.Time
-	ExpireTime time.Time
-	IV         float64
-	Calls      []Option
-	Puts       []Option
+	Symbol         string   `json:"symbol"`
+	Underlying     string   `json:"underlying"`
+	ExpirationDate string   `json:"expiration_date"`
+	Calls          []Option `json:"calls"`
+	Puts           []Option `json:"puts"`
 }
 
 type Option struct {
-	StrikePrice float64
-	BidPrice    float64
-	BidSize     int
-	AskPrice    float64
-	AskSize     int
+	StrikePrice float64 `json:"strike_price"`
+	BidPrice    float64 `json:"bid_price"`
+	BidSize     int     `json:"bid_size"`
+	BidAt       int64   `json:"bid_at"` // Unix timestamp in milliseconds
+	AskPrice    float64 `json:"ask_price"`
+	AskSize     int     `json:"ask_size"`
+	AskAt       int64   `json:"ask_at"`   // Unix timestamp in milliseconds
+	QuoteAt     int64   `json:"quote_at"` // Unix timestamp in milliseconds
 
-	IV    float64
-	Delta float64
-	Gamma float64
-	Vega  float64
-	Theta float64
+	IV              float64 `json:"iv"`
+	Delta           float64 `json:"delta"`
+	Gamma           float64 `json:"gamma"`
+	Vega            float64 `json:"vega"`
+	Theta           float64 `json:"theta"`
+	GreeksUpdatedAt int64   `json:"greeks_updated_at"` // Unix timestamp in milliseconds
 }
 
 func (c *Chain) SortByStrikePrice() {
